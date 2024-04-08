425a13c3ed5b50eb7881d6818b5c95cd415c4578
David <149296106+dmeleroandrade@users.noreply.github.com>
2024-04-08 11:41:08 +0000

    Generador de cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let arrayNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Q", "K"];
+let symbols = ["♠", "♣", "♥", "♦"];
+let intervalId;
+
+function generatedRandomCard() {
+  let SymbolsRandomIndex = Math.floor(Math.random() * symbols.length);
+  let NumberRandomIndex = Math.floor(Math.random() * arrayNumbers.length);
+
+  let randomNumbers = arrayNumbers[NumberRandomIndex];
+  let randomSymbols = symbols[SymbolsRandomIndex];
+  console.log("este es el numero random: ", randomNumbers);
+  console.log("este es el symbolo random: ", randomSymbols);
+
+  let symbolTop = document.querySelector("#symbolTop");
+  let symboloBottom = document.querySelector("#symbolBottom");
+  let numberMiddle = document.querySelector("#numberOne");
+
+  symbolTop.innerHTML = randomSymbols;
+  symboloBottom.innerHTML = randomSymbols;
+  numberMiddle.innerHTML = randomNumbers;
+}
+
+intervalId = setInterval(() => {
+  generatedRandomCard();
+}, 1000);
+
+const stopRandomCard = () => clearInterval(intervalId);
